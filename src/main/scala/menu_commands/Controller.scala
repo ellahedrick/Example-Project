e8fd62b8f94ea069328dc6dc950f145683a63abc
hansf123 <hans.fotta16@gmail.com>
2021-04-21 22:04:20 -0400

    updated scalaDocs files

--- src/main/scala/menu_commands/Controller.scala
+++ src/main/scala/menu_commands/Controller.scala
@@ -3,36 +3,37 @@ package menu_commands
 import scala.swing._
 
 /** The controller in the MVC architecture
-*  
-*  @constructor view the View in the MVC architecture
-*  @constructor model the Model in the MVC architecture
+*
+*  @constructor the controller in the MVC architecture
+*  @param view the View in the MVC architecture
+*  @param model the Model in the MVC architecture
 */
 class Controller(view: View, model: Model) {
-  
+
   /** shows the game by telling the view to update */
   def showGame = view.update
-  
+
   /** shuffles the deck by telling the model to shuffle */
   def shuffle = model.shuffle
-  
+
   /** shows the player order by telling the model to showPlayerOrder */
   def showPlayerOrder = model.showPlayerOrder
-  
+
   /** advances the player order by telling the model to advancePlayerOrder */
   def advancePlayerOrder = model.advancePlayerOrder
-  
+
   /** initializes the game by telling the model to initializeGame */
   def initializeGame = model.initializeGame
-  
+
   /** executes a turn by telling the model to doTurn */
   def doTurn = model.doTurn
-  
+
   /** simulates the whole game by telling the model to doGame */
   def doGame = model.doGame
-  
+
   /** shows the strategies by telling the model to showStrategies */
   def showStrategies = model.showStrategies
-  
+
   /** sets a given [Strategy] for a [Player] by telling the model to setStrategy */
   def setStrategy(player: Player, strategy: Strategy) = model.setStrategy(player, strategy)
 
